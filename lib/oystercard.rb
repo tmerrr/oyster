2bb1eb995573e9d8b90dbc0fe68ea61bb303ab86
Tabish Raza <tabishraza@gmail.com>
2017-10-30 16:45:06 +0000

    step 10 completed: charging for the journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,24 +3,21 @@ class Oystercard
   attr_reader :balance
 
   MAX_VALUE = 90
-  MIN_VALUE = 1
+  MIN_FARE = 1
 
-  def initialize(balance = MIN_VALUE)
+  def initialize(balance = MIN_FARE)
     @balance = balance
     @travel = false
   end
 
+  public
   def top_up(value)
     raise "Cannot exceed max balance of #{MAX_VALUE}" if (@balance + value > MAX_VALUE)
     @balance += value
   end
 
-  def deduct(value)
-    @balance -= value
-  end
-
   def touch_in
-    fail 'Insufficient Funds' if (balance <= MIN_VALUE)
+    fail 'Insufficient Funds' if (balance <= MIN_FARE)
     @travel = true
   end
 
@@ -30,6 +27,12 @@ class Oystercard
 
   def touch_out
     @travel = false
+    deduct(MIN_FARE)
+  end
+
+  private
+  def deduct(value)
+    @balance -= value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,22 +6,14 @@ describe Oystercard do
   describe "#balance" do
     context "When a new card is intialized" do
       it "has a balance of 0" do
-        expect(subject.balance).to eq 0
+        expect(subject.balance).to eq Oystercard::MIN_FARE
       end
     end
   end
 
   describe '#top_up' do
-    it { expect { subject.top_up(Oystercard::MAX_VALUE) }.to change { subject.balance }.by(Oystercard::MAX_VALUE) }
+    it { expect { subject.top_up(10) }.to change { subject.balance }.by(10) }
     it { expect { subject.top_up(Oystercard::MAX_VALUE + 1) }.to raise_error "Cannot exceed max balance of #{Oystercard::MAX_VALUE}" }
-  end
-
-  describe '#deduct' do
-    context 'when calling deduct(10) on a card with 50 as value' do
-      it 'returns balance as 40' do
-        expect { card.deduct(10) }.to change { card.balance }.by(-10)
-      end
-    end
   end
 
   describe '#touch_in' do
@@ -52,6 +44,13 @@ describe Oystercard do
       it "should change journery status" do
         card.touch_in
         expect { card.touch_out }.to change { card.in_journey? }
+      end
+    end
+
+    context 'when user touches out' do
+      it "should reduce by minimum fare" do
+        card.touch_out
+        expect { card.touch_out }.to change { card.balance }.by(-Oystercard::MIN_FARE)
       end
     end
   end
