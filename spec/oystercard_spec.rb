08f3823587b5df299a125d4470e86c93748c41f3
Tabish Raza <tabishraza@gmail.com>
2017-10-30 15:23:35 +0000

    step 4 complete: setup test to check balance + method to validate test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  describe "#balance" do
+    context "When a new card is intialized" do
+      it "has a balance of 0" do
+        expect(subject.balance).to eq 0
+      end
+    end
+  end
+
 
 
 end
